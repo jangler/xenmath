d51e3802a0818ef356b2348bc9ce8ce1c862473e
Casey Mulcahy <caseymulcahy@proton.me>
2023-04-18 10:48:29 -0400

    Add storage and dedupe to temperament search

--- src/search.clj
+++ src/search.clj
@@ -1,6 +1,8 @@
 (ns search
   "Searches for linear temperaments matching critera."
-  (:require [clojure.math :as math]
+  (:require [clojure.pprint :as pp]
+            [clojure.edn :as edn]
+            [clojure.math :as math]
             [clojure.math.combinatorics :as combo]
             [scale]
             [integer :refer [monzo prime-indices]]
@@ -9,12 +11,13 @@
 ; TODO: chromaticism
 ; TODO: mapping non-primes (9 and 15)
 
+(def save-path "data/found.edn")
 (def odd-limit 15)
 (def smallest-consonance 10/9)
 (def min-degrees-with-otonal-triads 1)
 (def min-mean-otonal-triads-per-degree 3)
 (def mos-size-range [5 9])
-(def error-limit 20)
+(def error-limit 15)
 (def min-primes-mapped 3)
 (def min-step-size 30)
 (def min-consonance-fraction 2/3)
@@ -327,142 +330,60 @@
                  t2
                  t))))))
 (defn search
+  [existing]
+  (let [existing-mappings (set (map :mapping existing))
+        existing-matrices (set (map scale-matrix existing))]
+    (loop [n 100]
+      (if (zero? n)
+        '()
+        (let [x (find-proper-generator)
+              finds (->> (for [n (x :mos-sizes)]
+                           (let [mapping (find-viable-mapping (x :generators) n)]
+                             {:mapping mapping
+                              :generators (x :generators)
+                              :mos-size n
+                              :pattern (scale/pattern-name
+                                        (first (scale/moses (x :generators) [n n])))}))
+                         (filter (fn [t]
+                                   (and (some? (t :mapping))
+                                        (not (contains? existing-mappings
+                                                        (t :mapping))))))
+                         (map #(->> %
+                                    (optimize 1000)
+                                    add-additional-mappings
+                                    strip-unused-mappings
+                                    (optimize 1000)))
+                         (filter #(not (contains? existing-matrices
+                                                  (scale-matrix %)))))]
+          (if (empty? finds)
+            (recur (dec n))
+            finds))))))
+
+(defn save-temperaments
+  "Write found temperaments to the save file."
+  [ts]
+  (spit save-path (with-out-str
+                    (pp/pprint ts))))
+
+(defn load-temperaments
+  "Load found temperaments from the save file."
   []
-  (loop [n 100]
-    (if (zero? n)
-      '()
-      (let [x (find-proper-generator)
-            finds (->> (for [n (x :mos-sizes)]
-                         (let [mapping (find-viable-mapping (x :generators) n)]
-                           {:mapping mapping
-                            :generators (x :generators)
-                            :mos-size n
-                            :pattern (scale/pattern-name
-                                      (first (scale/moses (x :generators) [n n])))}))
-                       (filter #(some? (% :mapping)))
-                       (map #(->> %
-                                  (optimize 1000)
-                                  add-additional-mappings
-                                  strip-unused-mappings
-                                  (optimize 1000))))]
-        (if (empty? finds)
-          (recur (dec n))
-          finds)))))
+  (edn/read-string (slurp save-path)))
 
 (comment
-  (def hedgehog-gens [600 165])
-  (def meantone
-    {:mapping [[1 2 4] [0 -1 -4]]
-     :generators [1200 503.7613]
-     :mos-size 7})
-  (def helmholtz-mapping [[1 2 -1] [0 -1 8]])
-  (error-stats consonances meantone)
-  (->> (mapped-scale hedgehog-gens 8)
-       (mapped-modes))
-  (->> (mapped-scale [1200 504] 7))
-  (->> (mapped-scale [1200 504] 7)
-       mapped-modes
-       (map #(mode-otonal-triads (meantone :mapping) %)))
-  (->> (mapped-scale [1200 498] 7)
-       mapped-modes
-       (map #(mode-otonal-triads helmholtz-mapping %)))
-  (def orgone
-    {:mapping [[1 nil nil 2 4] [0 nil nil 3 -2]]
-     :generators [1200 323.3717]
-     :mos-size 7})
-  (error-stats consonances orgone)
-  (->> (mapped-scale (orgone :generators) 7)
-       mapped-modes
-       (map #(mode-otonal-triads (orgone :mapping) %)))
+  ; to use this module, just alternate between evaluating these two expressions
+  (def ts (load-temperaments))
+  (save-temperaments (vec (concat ts (search ts))))
+  
+  ; diagnostics for running on newly found temperaments
+  (def t (last ts))
+  {:max-error ((error-stats consonances t) :max-error)
+   :scale-matrix (scale-matrix t)
+   :triads (scale-otonal-triads t)}
+  
+  ; other misc diagnostics
+  (filter #(not (meets-criteria? %)) ts)
 
-  (search)
-
-  (def oodako-ish
-    {:mapping [[3 3 3 4 6 nil] [0 4 9 10 10 nil]]
-     :generators [400 175.90928084154478]
-     :mos-size 9
-     :pattern "6L 3s"})
-  (scale-matrix oodako-ish)
-  (scale-otonal-triads oodako-ish)
-  (error-stats consonances oodako-ish)
-
-  (def dimipent
-    {:mapping [[4 6 9 nil nil nil] [0 1 1 nil nil nil]],
-     :generators [300 101.95509466107227],
-     :mos-size 8,
-     :pattern "4L 4s"})
-  (scale-matrix dimipent)
-  (scale-otonal-triads dimipent)
-  (error-stats consonances dimipent)
-
-  (def t
-    {:mapping [[2 nil 5 6 nil nil] [0 nil -1 -1 nil nil]],
-     :generators [600 213.73146960668458],
-     :mos-size 6,
-     :pattern "4L 2s"})
-  (scale-matrix t)
-  (scale-otonal-triads t)
-
-  (def orwell
-    {:mapping [[1 0 3 1 3 nil] [0 7 -3 8 2 nil]]
-     :generators [1200 271.1032645156822]
-     :mos-size 9
-     :pattern "4L 5s"})
-  (scale-matrix orwell)
-  (scale-otonal-triads orwell)
-  (error-stats consonances orwell)
-
-  (def sensi
-    {:mapping [[1 -1 -1 -2 nil nil] [0 7 9 13 nil nil]],
-     :generators [1200 443.52209743349977],
-     :mos-size 8,
-     :pattern "3L 5s"})
-  (scale-matrix sensi)
-  (scale-otonal-triads sensi)
-  (error-stats consonances sensi)
-
-  (def meantone
-    {:mapping [[1 2 4 nil nil nil] [0 -1 -4 nil nil nil]],
-     :generators [1200 503.4213338965872],
-     :mos-size 7,
-     :pattern "5L 2s"})
-  (scale-matrix meantone)
-  (scale-otonal-triads meantone)
-  (error-stats consonances meantone)
-
-  ; can't find a named augmented family temperament with a ~10/9 generator
-  (def t
-    {:mapping [[3 7 11 12 14 nil] [0 -5 -9 -8 -8 nil]],
-     :generators [400 179.3376238921682],
-     :mos-size 9,
-     :pattern "6L 3s"})
-  (scale-matrix t)
-  (scale-otonal-triads t)
-  (error-stats consonances t)
-
-  (def crepuscular
-    {:mapping [[2 2 3 4 6 nil] [0 5 7 7 4 nil]]
-     :generators [600 140.73473230631686]
-     :mos-size 8
-     :pattern "2L 6s"})
-  (scale-matrix crepuscular)
-  (scale-otonal-triads crepuscular)
-  (error-stats consonances crepuscular)
-
-  (def doublewide
-    {:mapping [[2 5 6 7 nil nil] [0 -4 -3 -3 nil nil]],
-     :generators [600 274.97119474976114],
-     :mos-size 6,
-     :pattern "4L 2s"})
-  (scale-matrix doublewide)
-  (scale-otonal-triads doublewide)
-  (error-stats consonances doublewide)
-
-  (def t
-    {:mapping [[2 nil 4 5 6 nil] [0 nil 2 2 3 nil]],
-     :generators [600 188.21718641121893],
-     :mos-size 8,
-     :pattern "6L 2s"})
-  (scale-matrix t)
-  (scale-otonal-triads t)
+  ; refilter
+  (save-temperaments (filter meets-criteria? ts))
   :rcf)
